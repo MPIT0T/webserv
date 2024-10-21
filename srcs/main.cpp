f6c28662c063be9871311a5dbeeed2ac0d1e130b
MPIT0T <mathispitot@gmail.com>
2024-10-21 16:26:55 +0200

    added argc argv

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,14 +6,16 @@
 /*   By: mpitot <mpitot@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 15:20:02 by mpitot            #+#    #+#             */
-/*   Updated: 2024/10/21 15:21:24 by mpitot           ###   ########.fr       */
+/*   Updated: 2024/10/21 15:41:08 by mpitot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
+	(void) argc;
+	(void) argv;
 	std::cout << "Hello World" << std::endl;
 	return (0);
 }
