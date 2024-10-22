d097fc71f01c73c41a543aef410edd1f191c1998
maxdegers <maxdegers@gmail.com>
2024-10-22 13:45:56 +0200

    adeded

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,19 +3,103 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpitot <mpitot@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: mbrousse <mbrousse@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 15:20:02 by mpitot            #+#    #+#             */
-/*   Updated: 2024/10/21 15:41:08 by mpitot           ###   ########.fr       */
+/*   Updated: 2024/10/22 13:41:50 by mbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+// CPP Includes
+# include <iostream>
+# include <iomanip>
+# include <sstream>
+# include <fstream>
+# include <string>
+# include <limits>
+# include <cstdio>
 
-int	main(int argc, char **argv)
+// CPP Containers
+# include <map>
+# include <set>
+# include <vector>
+# include <algorithm>
+# include <iterator>
+# include <list>
+# include <utility>
+
+// C Includes
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+# include <time.h>
+# include <limits.h>
+# include <errno.h>
+# include <dirent.h>
+
+// C System
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/time.h>
+
+// C Network
+# include <sys/socket.h>
+# include <sys/select.h>
+# include <arpa/inet.h>
+# include <netinet/in.h>
+
+
+#include <err.h>
+
+char response[] = "HTTP/1.1 200 OK\r\n"
+"Content-Type: text/html; charset=UTF-8\r\n\r\n"
+"<!DOCTYPE html><html><head><title>Bye-bye baby bye-bye</title>"
+"<style>body { background-color: #111 }"
+"h1 { font-size:4cm; text-align: center; color: black;"
+" text-shadow: 0 0 2mm red}</style></head>"
+"<body><h1>Goodbye, world!</h1></body></html>\r\n";
+
+int main()
 {
-	(void) argc;
-	(void) argv;
-	std::cout << "Hello World" << std::endl;
-	return (0);
+	int one = 1, client_fd;
+	struct sockaddr_in svr_addr, cli_addr;
+	socklen_t sin_len = sizeof(cli_addr);
+
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0)
+		err(1, "can't open socket");
+
+	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
+
+	int port = 8080;
+	svr_addr.sin_family = AF_INET;
+	svr_addr.sin_addr.s_addr = INADDR_ANY;
+	svr_addr.sin_port = htons(port);
+	printf("http://%s:%i\n", inet_ntoa(svr_addr.sin_addr), port);
+
+	if (bind(sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
+		close(sock);
+		err(1, "Can't bind");
+	}
+	listen(sock, 5);
+	while (1) {
+		client_fd = accept(sock, (struct sockaddr *) &cli_addr, &sin_len);
+		printf("clien addr : %s \n", inet_ntoa(cli_addr.sin_addr));
+		char buffer[2048];
+		int len = read(client_fd, buffer, 2047);
+		buffer[len] = 0;
+		printf("%s\n", buffer);
+		printf("got connection\n");
+
+		if (client_fd == -1) {
+			perror("Can't accept");
+			continue;
+		}
+
+		write(client_fd, response, sizeof(response) - 1);
+		close(client_fd);
+	}
 }
