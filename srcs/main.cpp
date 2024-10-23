70ab372b0148bd99d124040c1f271d4f7496bfd2
maxdegers <maxdegers@gmail.com>
2024-10-23 18:18:19 +0200

    add page web working

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,16 +6,100 @@
 /*   By: mbrousse <mbrousse@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 15:20:02 by mpitot            #+#    #+#             */
-/*   Updated: 2024/10/22 14:35:50 by mbrousse         ###   ########.fr       */
+/*   Updated: 2024/10/23 18:10:50 by mbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+// CPP Includes
+# include <iostream>
+# include <iomanip>
+# include <sstream>
+# include <fstream>
+# include <string>
+# include <limits>
+# include <cstdio>
 
-int	main(int argc, char **argv)
+// CPP Containers
+# include <map>
+# include <set>
+# include <vector>
+# include <algorithm>
+# include <iterator>
+# include <list>
+# include <utility>
+
+// C Includes
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+# include <time.h>
+# include <limits.h>
+# include <errno.h>
+# include <dirent.h>
+
+// C System
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/time.h>
+
+// C Network
+# include <sys/socket.h>
+# include <sys/select.h>
+# include <arpa/inet.h>
+# include <netinet/in.h>
+
+#include <err.h>
+
+int main()
 {
-	(void) argc;
-	(void) argv;
-	std::cout << "Hello World !" << std::endl;
-	return (0);
+	int one = 1, client_fd;
+	struct sockaddr_in svr_addr, cli_addr;
+	socklen_t sin_len = sizeof(cli_addr);
+
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0)
+		err(1, "can't open socket");
+
+	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
+	
+	std::ifstream file("www/main/index.html");
+	if (!file.is_open()) {
+		err(1, "Can't open index.html");
+	}
+	std::string http_header = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n";
+	std::string response = http_header + std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+	file.close();
+	
+
+	int port = 4242;
+	svr_addr.sin_family = AF_INET;
+	svr_addr.sin_addr.s_addr = INADDR_ANY;
+	svr_addr.sin_port = htons(port);
+	printf("http://%s:%i\n", inet_ntoa(svr_addr.sin_addr), port);
+
+	if (bind(sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
+		close(sock);
+		err(1, "Can't bind");
+	}
+	listen(sock, 5);
+	while (1) {
+		client_fd = accept(sock, (struct sockaddr *) &cli_addr, &sin_len);
+		printf("clien addr : %s \n", inet_ntoa(cli_addr.sin_addr));
+		char buffer[2048];
+		int len = recv(client_fd, buffer, 2048, 0);
+		buffer[len] = 0;
+		printf("%s\n", buffer);
+		printf("got connection\n");
+
+		if (client_fd == -1) {
+			perror("Can't accept");
+			continue;
+		}
+
+		write(client_fd, response.c_str(), response.size());
+		close(client_fd);
+	}
 }
