1cfb2a62ea68eea77fb0dd48ce24d4fef2eff616
maxdegers <maxdegers@gmail.com>
2024-10-29 16:54:53 +0100

    route.hpp

--- srcs/manage_args/Route.hpp
+++ srcs/manage_args/Route.hpp
@@ -15,16 +15,32 @@ private:
 
 public:
 	// Constructors
+
 	Route( void );
 	Route( const Route &src );
 	Route &operator=( const Route &src );
 	
 	// Destructor
+	
 	~Route();
 	
 	// Setters
 
+	void setDirectoryListing(bool directory_listing);
+	void setPath(const std::string &path);
+	void setRoot(const std::string &root);
+	void setHttpRedirect(const std::string &http_redirect);
+	void setAllowMethods(const std::map<std::string, bool> &allow_methods);
+	void setCgi(const std::map<std::string, std::string> &cgi);
+
 	// Getters
+
+	bool getDirectoryListing() const;
+	std::string getPath() const;
+	std::string getRoot() const;
+	std::string getHttpRedirect() const;
+	std::map<std::string, bool> getAllowMethods() const;
+	std::map<std::string, std::string> getCgi() const;
 };
 
 #endif // Route_HPP
